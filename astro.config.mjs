ed14933159e88b15587f92ad04f99d7624742247
harleylara <harley.lara@outlook.com>
2026-06-30 13:08:35 +0200

    update to tailwind vite plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +12,5 @@ export default defineConfig({
         }
     },
     site: `https://bookmarks.harleylara.com`,
-    integrations: [tailwind()]
+    integrations: [tailwindcss()]
 });
